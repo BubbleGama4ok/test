b83906a5d19442a74622be3ae7200c84f64c86a3
Евгений <evgenykulmanov@yandex.ru>
2023-10-02 21:33:09 +0300

    deque

--- guess-number-task/teach.py
+++ guess-number-task/teach.py
@@ -1,16 +1,20 @@
-students = [('Ivanov',1),('Smirnov',4),('Petrov',3),('Kuznetsova',1),
+#students = [('Ivanov',1),('Smirnov',4),('Petrov',3),('Kuznetsova',1),
             ('Nikitina',2),('Markov',3),('Pavlov',2)]
 
 
-from collections import defaultdict
+#from collections import defaultdict
 #создаем пустой объект Counter
-groups = defaultdict(list)
+#groups = defaultdict(list)
 
-for student, group in students:
+#for student, group in students:
     groups[group].append(student)
 
-print(groups)
+#print(groups)
 # Будет напечатано:
 # Counter({'red':})
-print(groups[3077])
+#print(groups[3077])
 
+
+data = [('Ivan', 19),('Mark', 25),('Andrey', 23),('Maria', 20)]
+client_ages = dict(data)
+print(client_ages)
